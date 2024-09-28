2693a7e3769220e3a8598f27e034cc3e48adc049
kgarri <black.hat.kg@gmail.com>
2024-09-28 10:56:50 -0500

    final commit

--- rdt/UDPPingerServerSR.py
+++ rdt/UDPPingerServerSR.py
@@ -7,31 +7,33 @@ serverSocket.settimeout(2.0)
 serverSocket.bind(('', 12000))
 seq_num = 0
 window = [] 
-r = 0
 n = 2
+start_index = 0
 while True:
 
     packet = seq_num
-    print(seq_num)
     try:
         message,address = serverSocket.recvfrom(1024)  
         message = message.decode('utf-8').split(',')
-        print(message)
+        cur_seq = int(message[0])
         if len(window)<n:
-            window.append(int(message[0]))
+            window.append(cur_seq)
         else:
-            if window[n-1] == int(message[0]):
-                seq_num = int(message[0])
-                window.append(int(message[0]))
-            if seq_num == r:
-                for seq in range(r,n):
+            if window[n-1] == cur_seq:
+                for seq in range(start_index,n):
                     message = str(window[seq]) + ', ACK'
                     serverSocket.sendto(message.encode(),address)
-                    r+=1
                     n+=1
+                    start_index +=1
             else:
-                window.append(int(message[0]))
-                message = str(seq_num) + ', ACK'
+                found = False
+                for seq in window:
+                    if seq == message[0]:
+                        found = True
+                        break
+                if found == False:
+                    window.append(cur_seq)
+                message = str(cur_seq) + ', ACK'
                 serverSocket.sendto(message.encode(),address)
 
     except timeout:
